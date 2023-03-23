95d97427d13779306277ede729ac305adf8f962a
Ashir Amin <ashiramin123@gmail.com>
2023-03-23 09:54:53 -0500

    init commit

---
+++ spec/controllers/test_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe TestController do
+    describe '#index' do
+      subject { get :index }
+          it do
+            is_expected.to have_http_status(200)
+          end
+    end
+end
